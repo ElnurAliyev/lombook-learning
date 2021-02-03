d4256be12b85e6d2844ce2d9f8b10e3d837f3056
Elnur Aliyev <aelnur@guavapay.com>
2021-02-03 21:43:57 +0400

    initial

---
+++ src/main/java/com/learning/lombooklearnig/LombookLearnigApplication.java
@@ -0,0 +1,13 @@
+package com.learning.lombooklearnig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombookLearnigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombookLearnigApplication.class, args);
+	}
+
+}
